848ec551b1f590100149d285e4d7fedc5cb167ba
Benjamin Schwartz <bs4308@nyu.edu>
2022-08-23 16:36:13 -0400

    Weird attribute error

--- skeleton/lib/00_attr_accessor_object.rb
+++ skeleton/lib/00_attr_accessor_object.rb
@@ -1,5 +1,18 @@
 class AttrAccessorObject
   def self.my_attr_accessor(*names)
-    # ...
+    names.each do |attribute|
+      attribute = attribute.to_s
+
+      #define getter method
+      define_method(attribute) do
+        instance_variable_get("@#{attribute}")
+      end
+      #define setter method
+
+      define_method("#{attribute}=") do |val|
+        instance_variable_set("@#{attribute}", val)
+      end
+    end
+
   end
 end
--- skeleton/lib/01_sql_object.rb
+++ skeleton/lib/01_sql_object.rb
@@ -1,22 +1,55 @@
 require_relative 'db_connection'
 require 'active_support/inflector'
+require 'byebug'
 # NB: the attr_accessor we wrote in phase 0 is NOT used in the rest
 # of this project. It was only a warm up.
 
 class SQLObject
   def self.columns
-    # ...
+    return @colums if @columns
+    # table_name = @table_name
+    arr = DBConnection.execute2(<<-SQL)
+      SELECT
+        *
+      FROM
+        #{self.table_name}
+    SQL
+    # debugger
+    @columns = arr[0].map!(&:to_sym)
+  end
+
+  def attributes
+    @attributes ||= {}
   end
 
   def self.finalize!
+    columns.each do |attribute|
+      # attribute = attribute.to_s
+
+      #define getter method
+      define_method(attribute) do
+        # instance_variable_get("@#{attribute}")
+
+        self.attributes[attribute]
+      end
+
+      #define setter method
+      define_method("#{attribute}=") do |val|
+        # instance_variable_set("@#{attribute}", val)
+        # debugger
+        self.attributes[attribute] = val
+      end
+    end
+
+
   end
 
   def self.table_name=(table_name)
-    # ...
+    @table_name= table_name
   end
 
   def self.table_name
-    # ...
+    @table_name || self.to_s.tableize
   end
 
   def self.all
@@ -35,9 +68,6 @@ class SQLObject
     # ...
   end
 
-  def attributes
-    # ...
-  end
 
   def attribute_values
     # ...
